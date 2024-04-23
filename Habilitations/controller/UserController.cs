cc010b24ae9445cc18ce7dff043354e18ea52480
Julian <julian.vieira-gebcyznski@etu.formation-aftec.com>
2024-04-23 09:41:57 +0200

    User, vue sur les infos du User.

---
+++ Habilitations/controller/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Habilitations.model;
+
+namespace Habilitations.controller
+{
+    public class UserController
+    {
+        public User GetUserById(int userId)
+        {
+            // Logique pour récupérer l'utilisateur depuis une source de données
+            // Par exemple, retourner un utilisateur simulé
+            return new User { Id = userId, Name = "Jean Dupont", Email = "jean.dupont@example.com" };
+        }
+    }
+}
---
+++ Habilitations/view/UserDetailsForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Habilitations.model;
+
+namespace Habilitations.view
+{
+    public partial class UserDetailsForm : Form
+    {
+        public UserDetailsForm(User user)
+        {
+            InitializeComponent();
+            lblName.Text = user.Name;
+            lblEmail.Text = user.Email;
+        }
+    }
+}
